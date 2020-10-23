d76e3b8240c9af0bcf8c8e13ce78ce9557399509
David <62627903+1nspir3d@users.noreply.github.com>
2020-10-23 22:29:48 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = 
+function check(str, bracketsConfig) {
+  const stack = [];
+    const brackets = Object.fromEntries(bracketsConfig);
+
+    for (const el of str.split``) {
+        if (stack[stack.length - 1] === el) {
+            stack.pop();
+        } else if (brackets[el]) {
+            stack.push(brackets[el]);
+        } else {
+            return false;
+        }
+    }
+
+    return stack.length === 0;
+};
+
+
